9b1d71a97cbbf85c4e9f636b8478514debf99bcc
eliasjuk <eliasp.juk@hotmail.com>
2024-10-17 20:20:20 -0300

    Criado a classe usuario

--- src/main/java/com/sm/models/Usuario.java
+++ src/main/java/com/sm/models/Usuario.java
@@ -3,37 +3,37 @@ package com.sm.models;
 import java.time.LocalDateTime;
 
 public class Usuario {
-    private String user;
-    private String senha;
-    private LocalDateTime dataCriacao;
+  private String user;
+  private String senha;
+  private LocalDateTime dataCriacao;
 
-    public Usuario(String user, String senha) {
-        this.user = user;
-        this.senha = senha;
-        this.dataCriacao = LocalDateTime.now(); // Define a data de criação como agora
-    }
+  public Usuario(String user, String senha) {
+    this.user = user;
+    this.senha = senha;
+    this.dataCriacao = LocalDateTime.now(); // Define a data de criação como agora
+  }
 
-    public String getUser() {
-        return user;
-    }
+  public String getUser() {
+    return user;
+  }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
+  public void setUser(String user) {
+    this.user = user;
+  }
 
-    public String getSenha() {
-        return senha;
-    }
+  public String getSenha() {
+    return senha;
+  }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
 
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
-    }
+  public LocalDateTime getDataCriacao() {
+    return dataCriacao;
+  }
 
-    public void setDataCriacao(LocalDateTime dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
+  public void setDataCriacao(LocalDateTime dataCriacao) {
+    this.dataCriacao = dataCriacao;
+  }
 }
