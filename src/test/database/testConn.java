7ad60627831b4fca527f53c042aef03b0367db83
eliasjuk <eliasp.juk@hotmail.com>
2024-10-17 18:15:13 -0300

    Testar Conexão

---
+++ src/test/database/testConn.java
@@ -0,0 +1,39 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.sm.database.DatabaseConnection;
+
+public class testConn {
+  private Connection conn;
+
+  // Método para abrir a conexão com o banco usando a classe DatabaseConn
+  public void connect() throws SQLException {
+    conn = DatabaseConnection.getConnection();
+    System.out.println("Conexão aberta com sucesso.");
+  }
+
+  // Método para fechar a conexão usando a classe DatabaseConn
+  public void close() throws SQLException {
+    DatabaseConnection.closeConnection();
+    System.out.println("Conexão fechada com sucesso.");
+  }
+
+  public static void main(String[] args) {
+    testConn test = new testConn();
+
+    try {
+      // Abrindo a conexão
+      test.connect();
+    } catch (SQLException e) {
+      System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+    } finally {
+      try {
+        // Fechando a conexão
+        test.close();
+      } catch (SQLException e) {
+        System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+      }
+    }
+  }
+}
