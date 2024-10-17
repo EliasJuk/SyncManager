389568146361ab59c92c0208c76d79955fe511aa
eliasjuk <eliasp.juk@hotmail.com>
2024-10-17 19:03:58 -0300

    classe Usuario

---
+++ src/main/java/com/sm/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.sm.models;
+
+import java.time.LocalDateTime;
+
+public class Usuario {
+    private String user;
+    private String senha;
+    private LocalDateTime dataCriacao;
+
+    public Usuario(String user, String senha) {
+        this.user = user;
+        this.senha = senha;
+        this.dataCriacao = LocalDateTime.now(); // Define a data de criação como agora
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
