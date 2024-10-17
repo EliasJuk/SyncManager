0275d8221d318692e0cf2071423fec1bcfb3879a
eliasjuk <eliasp.juk@hotmail.com>
2024-10-17 20:18:59 -0300

    carregar janela pelas rotas

--- src/main/java/com/sm/App.java
+++ src/main/java/com/sm/App.java
@@ -3,16 +3,15 @@ package com.sm;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+public class App extends Application {
+  
+  @Override
+  @SuppressWarnings("exports")
+  public void start(Stage primaryStage) throws Exception {
+    Routes.Login(getClass(), primaryStage);
+  }
 
-public class App extends Application {    
-    @Override
-    @SuppressWarnings("exports")
-    public void start(Stage primaryStage) throws Exception{
-        Routes.Login(getClass(), primaryStage);
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
