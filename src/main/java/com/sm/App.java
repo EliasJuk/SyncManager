e709482c35d8a83da7713453f372e72a2275043a
eliasjuk <eliasp.juk@hotmail.com>
2024-10-17 16:46:44 -0300

    add Login a classe de rotas

--- src/main/java/com/sm/App.java
+++ src/main/java/com/sm/App.java
@@ -1,23 +1,14 @@
 package com.sm;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class App extends Application {
-    
+public class App extends Application {    
     @Override
+    @SuppressWarnings("exports")
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/sm/views/viewLogin.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene login = new Scene(root);
-
-        primaryStage.setTitle("Login");
-        primaryStage.setScene(login);
-        primaryStage.show();
+        Routes.Login(getClass(), primaryStage);
     }
 
 
