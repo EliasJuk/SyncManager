7d2ce250f5a37d6213d752e9fcbf29e888daea22
eliasjuk <eliasp.juk@hotmail.com>
2024-10-31 17:47:38 -0300

    atualizar modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,9 @@ module com.sm {
 
     opens com.sm to javafx.fxml;
     opens com.sm.controllers to javafx.fxml;
+    opens com.sm.utils to javafx.base;
+    opens com.sm.controllers.colaboradores to javafx.fxml;
+
+    //opens com.sm.models.pessoas to javafx.fxml;
+    //opens com.sm.controllers.colaboradores to javafx.fxml;
 }
