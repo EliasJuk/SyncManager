e1623f287781dd41ba86269f9a483a0a31107489
eliasjuk <eliasp.juk@hotmail.com>
2024-11-03 09:07:34 -0300

    add circuitos e circuitos_componentes as migrations

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+-- Cria o banco de dados
 CREATE DATABASE syncmanager
   WITH
   OWNER = postgres
@@ -8,9 +9,10 @@ CREATE DATABASE syncmanager
   CONNECTION LIMIT = -1
   IS_TEMPLATE = False;
 
--- Conecta ao banco de dados syncmanager antes de criar a tabela.
+-- Conecta ao banco de dados syncmanager
 \c syncmanager
 
+-- Cria a tabela de componentes
 CREATE TABLE componentes (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
@@ -18,4 +20,24 @@ CREATE TABLE componentes (
   preco REAL NOT NULL,
   descricao TEXT,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Cria a tabela de circuitos
+CREATE TABLE circuitos (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  ctf TEXT NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Cria a tabela de relação entre circuitos e componentes
+CREATE TABLE circuitos_componentes (
+  circuito_id INTEGER NOT NULL,
+  componente_id INTEGER NOT NULL,
+  componente_ctf TEXT NOT NULL,
+  circuito_ctf TEXT NOT NULL,
+  quantidade INTEGER NOT NULL,
+  PRIMARY KEY (circuito_id, componente_id),
+  FOREIGN KEY (circuito_id) REFERENCES circuitos(id) ON DELETE CASCADE,
+  FOREIGN KEY (componente_id) REFERENCES componentes(id) ON DELETE CASCADE
 );
