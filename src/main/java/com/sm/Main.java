ef8d75ef2a30c2719f73941ff212087ce597b3dd
eliasjuk <eliasp.juk@hotmail.com>
2024-11-03 16:43:16 -0300

    indentação

--- src/main/java/com/sm/Main.java
+++ src/main/java/com/sm/Main.java
@@ -1,7 +1,7 @@
 package com.sm;
 
 public class Main {
-    public static void main(String[] args) {
-        App.main(args);
-    }
+	public static void main(String[] args) {
+		App.main(args);
+	}
 }
