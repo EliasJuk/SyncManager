15fc2cfe4550ec4c48fc7f794f06283cbd09be57
eliasjuk <eliasp.juk@hotmail.com>
2024-11-16 19:55:21 -0300

    add nova coluna a tabela

--- src/main/java/com/sm/controllers/colaboradores/presencaController.java
+++ src/main/java/com/sm/controllers/colaboradores/presencaController.java
@@ -1,9 +1,17 @@
 package com.sm.controllers.colaboradores;
 
 import java.net.*;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
-import com.sm.utils.Table;
+import com.sm.dao.sqlite.empresa.BUDAO;
+import com.sm.dao.sqlite.empresa.SetorDAO;
+import com.sm.dao.sqlite.pessoas.ColaboradorDAO;
+import com.sm.models.Table;
+import com.sm.models.empresa.BU;
+import com.sm.models.empresa.Setor;
+import com.sm.utils.callMessage;
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -14,23 +22,28 @@ import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class presencaController implements Initializable {
+  private final ColaboradorDAO colaboradorDAO = new ColaboradorDAO();
+  private final callMessage messageHandler = new callMessage();
+  LocalDate selectedDate = null;
 
   // COMPNENTES REFRENTE A MENUS DE SELEÇÃO
   @FXML
-  private ComboBox<?> buNameBox;
-
+  private ComboBox<BU> buNameBox;
+  @FXML
+  private ComboBox<String> setorNameBox;
   @FXML
   private DatePicker dpDataSelect;
-
-  @FXML
-  private ComboBox<?> setorNameBox;
-
   @FXML
   private TextField tfNomeColaborador;
+  @FXML
+  private Button btnSalvar;
 
   @FXML
   void cbBUOnAction(ActionEvent event) {
-
+    BU selectedBU = buNameBox.getSelectionModel().getSelectedItem();
+    if (selectedBU != null) {
+      carregarSetores(selectedBU.getId());
+    }
   }
 
   @FXML
@@ -40,7 +53,12 @@ public class presencaController implements Initializable {
 
   @FXML
   void dpSelectDataOnAction(ActionEvent event) {
-
+    LocalDate selectedDate = dpDataSelect.getValue();
+    if (selectedDate != null) {
+      System.out.println("Data selecionada: " + selectedDate);
+    } else {
+      System.out.println("Nenhuma data selecionada.");
+    }
   }
 
   @FXML
@@ -48,11 +66,21 @@ public class presencaController implements Initializable {
 
   }
 
+  @FXML
+  void btnSalvarOnAction(ActionEvent event) {
+    if(selectedDate != null){
+
+    }else{
+      messageHandler.showAlert(Alert.AlertType.WARNING, "AVISO!", "SELECIONE UMA DATA", 
+      "SELECIONE UMA DATA POR FAVOR");
+    }
+  }
+
   // COMPONENTES REFERENTE A TABELA
   @FXML
   TableView<Table> tableID;
   @FXML
-  private TableColumn<?, ?> iMatriculaBox;
+  private TableColumn<Table, String> iMatriculaBox;
   @FXML
   TableColumn<Table, String> iFirstName;
   @FXML
@@ -64,8 +92,14 @@ public class presencaController implements Initializable {
   @FXML
   TableColumn<Table, Boolean> iAtestadoBox;
 
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    //CARREGA O COMBOBOX DE BU
+    carregarBUComboBox();
+
+    // CARREGA A TABELA DE PRESENÇA
+    iMatriculaBox.setCellValueFactory(new PropertyValueFactory<>("rMatricula"));
     iFirstName.setCellValueFactory(new PropertyValueFactory<>("rFirstName"));
     iLastName.setCellValueFactory(new PropertyValueFactory<>("rLastName"));
 
@@ -80,8 +114,34 @@ public class presencaController implements Initializable {
 
     // Populando a tabela com os dados de teste
     tableID.setItems(FXCollections.observableArrayList(
-      new Table("Airi", "Matsumoto", true, false, false),
-      new Table("Misaki", "Aoki", false, true, false),
-      new Table("Emi", "Oshida", false, false, true)));
+        new Table("200001","Airi", "Matsumoto", true, false, false),
+        new Table("200002","Misaki", "Aoki", false, true, false),
+        new Table("200003","Emi", "Oshida", false, false, true)));
+  }
+
+  private void carregarBUComboBox() {
+    BUDAO buDAO = new BUDAO();
+    try {
+      buDAO.connect();
+      List<BU> buList = buDAO.findAll();
+      buNameBox.setItems(FXCollections.observableArrayList(buList));
+      buDAO.close();
+    } catch (Exception e) {
+      messageHandler.showAlert("Erro", "Erro ao carregar a lista de BUs:", e.getMessage());
+    }
+  }
+
+  private void carregarSetores(int buId) {
+    SetorDAO setorDAO = new SetorDAO();
+    try {
+      setorDAO.connect();
+      List<Setor> setores = setorDAO.findByBUId(buId);
+      List<String> nomesSetores = setores.stream().map(Setor::getNome).collect(Collectors.toList());
+
+      setorNameBox.setItems(FXCollections.observableArrayList(nomesSetores));
+      setorDAO.close();
+    } catch (Exception e) {
+      messageHandler.showAlert("Erro", "Erro ao carregar a lista de Setores:", e.getMessage());
+    }
   }
 }
